02e12fdf7ea80d0f63f24abcd26b8e36fa45a0fa
Simon Meister <simon.meister@wagner.ch>
2026-01-12 20:50:48 +0000

    🔒️ add gpg config

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -3,3 +3,6 @@
 find $OMD_ROOT/tmp/ -name "*.pid" -exec rm {} \;
 
 source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart
+
+# Add git config for gpg commit signing
+git config --global gpg.format ssh && git config --global commit.gpgsign true
