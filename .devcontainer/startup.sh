2ed143391b9d19e4633db353d7eed059633bd4d8
WYSR <18366776+dampfhamm3r@users.noreply.github.com>
2026-01-13 09:06:43 +0000

    🧹 remove ssh gpg format

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -5,4 +5,4 @@ find $OMD_ROOT/tmp/ -name "*.pid" -exec rm {} \;
 source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart
 
 # Add git config for gpg commit signing
-git config --global gpg.format ssh && git config --global commit.gpgsign true
+git config --global commit.gpgsign true
