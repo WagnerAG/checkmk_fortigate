413ddae7d16e7836681f83c466e37515d99aaee9
WYSR <18366776+dampfhamm3r@users.noreply.github.com>
2026-01-21 15:31:50 +0000

    🐛 fix container gpg config

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,5 +4,7 @@ find $OMD_ROOT/tmp/ -name "*.pid" -exec rm {} \;
 
 source /omd/sites/cmk/.profile && $OMD_ROOT/bin/omd restart
 
+echo "export GPG_TTY=$(tty)" >> /omd/sites/cmk/.bashrc
+
 # Add git config for gpg commit signing
 git config --global commit.gpgsign true
