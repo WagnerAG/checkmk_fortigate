6597d23a68bc33ac9fabc9078071858a0e6c927a
Simon Meister <simon.meister@wagner.ch>
2026-02-24 13:42:23 +0000

    add new template

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -5,10 +5,10 @@ WORKSPACE=$(pwd)
 
 ln -sv $WORKSPACE/cmk_addons/plugins/$NAME $OMD_ROOT/local/lib/python3/cmk_addons/plugins/$NAME
 
-rm -rf $OMD_ROOT/local/lib/python3/cmk
+rm -rfv $OMD_ROOT/local/lib/python3/cmk
 ln -sv $WORKSPACE/lib $OMD_ROOT/local/lib/python3/cmk
 
-rm -rf $OMD_ROOT/local/share/check_mk
+rm -rfv $OMD_ROOT/local/share/check_mk
 ln -sv $WORKSPACE/plugins_legacy $OMD_ROOT/local/share/check_mk
 
 source /omd/sites/cmk/.profile && echo 'cmkadmin' | /omd/sites/cmk/bin/cmk-passwd -i cmkadmin
