4c44301ae9a9375de225fc95ec48ee81cd46a56d
user01010111 <12504630+user01010111@users.noreply.github.com>
2026-05-07 01:23:55 +1200

    Fix Checkmk 2.5 compatibility (#32)
    * Fix Checkmk 2.5 compatibility
    
    * Add git safe.directory configuration step
    
    Added a step to mark the workspace as safe for git operations.
    
    * Simplify gitleaks workflow by removing permissions
    
    Removed unnecessary permissions declaration for contents.
    
    * Remove pull_request trigger from gitleaks.yml
    
    Removed pull_request trigger from gitleaks workflow.
    
    ---------
    
    Co-authored-by: MEIS <17747347+7meis@users.noreply.github.com>

--- lib/gui/plugins/views/fortios_inventory.py
+++ lib/gui/plugins/views/fortios_inventory.py
@@ -27,7 +27,7 @@ from cmk.gui.type_defs import (
     VisualLinkSpec,
 )
 from cmk.gui.views.store import multisite_builtin_views
-from cmk.utils.user import UserId
+from cmk.ccc.user import UserId
 
 multisite_builtin_views.update(
     {
@@ -111,7 +111,7 @@ multisite_builtin_views.update(
                 "has_inv": {"is_has_inv": "1"},
             },
             "owner": UserId.builtin(),
-            "megamenu_search_terms": [],
+            "main_menu_search_terms": [],
         },
     }
 )
